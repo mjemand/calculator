56195ad48df42475a2e53121469636efc9671c95
mjemand <mechle.tamas@gmail.com>
2020-12-01 20:00:50 +0100

    keys

---
+++ js/main.js
@@ -0,0 +1,52 @@
+const calculator = {
+    displayValue: '0',
+    firstOperand: null,
+    waitingForSecondOperand: false,
+    operator: null,
+  };
+  
+function inputDigit(digit) {
+    const { displayValue } = calculator;
+    calculator.displayValue = displayValue === '0' ? digit : displayValue + digit;
+}
+
+function inputDecimal(dot) {
+    if (!calculator.displayValue.includes(dot)) {
+      calculator.displayValue += dot;
+    }
+  }
+  
+
+function updateDisplay() {
+    const display = document.querySelector('.calculator-screen');
+    display.value = calculator.displayValue;
+}
+  
+updateDisplay();
+
+const keys = document.querySelector('.container');
+keys.addEventListener('click', (event) => {
+  const { target } = event;
+  if (!target.matches('button')) {
+    return;
+  }
+
+  if (target.classList.contains('operator')) {
+    console.log('operator', target.value);
+    return;
+  }
+
+  if (target.classList.contains('decimal')) {
+    inputDecimal(target.value);
+	updateDisplay();
+    return;
+  }
+
+  if (target.classList.contains('all-clear')) {
+    console.log('clear', target.value);
+    return;
+  }
+
+  inputDigit(target.value);
+  updateDisplay();
+});
